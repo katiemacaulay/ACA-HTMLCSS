3365abd2e68332f862803b680af59adab0b9a36b
katiemacaulay <katiemmacaulay@gmail.com>
2018-08-26 22:06:20 -0500

    dom-practice;tutoringhelp

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,31 +1,74 @@
 'use strict';
-
-  window.onload = function(){
-
-    let list = document.getElementsByTagName('li');
-
-    // window.alert('You have ' + list.length + ' items in your shopping cart')
-
-    var h1 = document.querySelector('h1')
-      h1.insertAdjacentHTML('afterEnd',
-      '<h1>You have ' + list.length + ' items in your shopping cart</h1>');
-
-    // Another way of adding a tag
-      // let newh1 = document.createElement('h1');
-      // newh1.innerHTML="You have " + list.length + " items in your shopping cart"
-      // document.querySelector('h1').appendChild(newh1);
-
-        let input = document.createElement('input');
-        input.innerHTML = "";
-        document.querySelector('ul').appendChild(input);
-
-        let button = document.querySelector('ul');
-        // Possibly another way to make this button work:
-        // button.setAttribute("type", "button");
-        button.insertAdjacentHTML('beforeEnd',
-        '<button onclick="myFunction()"> Add to List </button>');
-
-        function myFunction(){
-          ul.push(input);
-        }
+//  document.addEventListener("DOMContentLoaded", function(event) {
+// });
+window.onload=function(){
+ //Job number 1: Alert the user to how many list items there on the page
+  let listCountMessage = setListMessage();
+  alert(listCountMessage);
+ // Job number 2: Add a <h2> under the title that says 'You have _ items in your shopping cart'
+  createListCountHeaderElement();
+  updateListCountHeaderMessage();
+ //Job 3
+  createNewItemTextInput();
+ // part of Job 4
+  addRemoveButtonstoExistingItems();
+   }
+ // used for Job 1 and 2
+     function setListMessage(){
+      let list = document.getElementById("shopping-list");
+      return "You have "+ list.children.length + " items in your shopping cart.";
+    }
+ // used for Job 2
+     function createListCountHeaderElement(message){
+      let listHeading= document.createElement("h2");
+      document.getElementsByTagName("h1")[0].appendChild(listHeading);
+      listHeading.setAttribute("id", "list-heading");
+}
+ //used for Job 2,3,and 4
+     function updateListCountHeaderMessage (){
+      let listAlert = setListMessage();
+      let listHeading = document.getElementById("list-heading");
+      listHeading.innerHTML = listAlert;
+}
+ // Used for job 4
+     function addRemoveButtonstoExistingItems(){
+      let list=document.getElementById("shopping-list");
+      for (let i=0; i<list.children.length; i++){
+        addRemoveButton(list.children[i]);
+  }
+}
+ //used for jobs 3 & 4
+     function addRemoveButton (appendElement){
+      console.log(appendElement);
+      let removeButton = document.createElement("button");
+      removeButton.innerHTML = "Remove";
+      removeButton.onclick = deleteItem;
+      appendElement.appendChild(removeButton);
+      updateListCountHeaderMessage();
+}
+ //used for job 3
+     function createNewItemTextInput(){
+      let textField = document.getElementById("item-text");
+      textField.setAttribute("placeholder", "type item to add");
+      let addButton = document.getElementById("add-button");
+      addButton.innerHTML="Add Item";
+      addButton.onclick=addItem;
+      document.getElementById("add-item").appendChild(textField);
+      document.getElementById("add-item").appendChild(addButton);
+}
+ // job 3 Create the ability to add more items to the list using JS
+     function addItem(event){
+      event.preventDefault();
+      let newItemText = document.getElementById("item-text").value;
+      let newItem=document.createElement("li");
+      newItem.innerHTML=newItemText;
+      document.getElementById("shopping-list").appendChild(newItem);
+      updateListCountHeaderMessage();
+       addRemoveButton(newItem);
+      document.getElementById('add-item').reset();
+}
+ // job 4 Create the ability to remove an item from the cart using js
+     function deleteItem(){
+      this.parentNode.remove(this);
+      updateListCountHeaderMessage();
 }
